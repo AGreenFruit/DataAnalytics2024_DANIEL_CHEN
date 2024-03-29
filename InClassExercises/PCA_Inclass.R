ffd48d637d225a91954d4895135a6cb3137bb7d6
Daniel Chen <85917977+AGreenFruit@users.noreply.github.com>
2024-03-29 14:35:40 -0400

    Added lab7 and PCA wine

--- InClassExercises/PCA_Inclass.R
+++ InClassExercises/PCA_Inclass.R
@@ -52,3 +52,19 @@ pr.var
 
 pve = pr.var/sum(pr.var)
 pve
+
+# wine_data <- read.table("http://archive.ics.uci.edu/dataset/109/wine/wine.data", sep = ",")
+wine_data <- read.csv("C:/Users/danie/Documents/DataAnalytics2024_DANIEL_CHEN/InClassExercises/wine.data")
+head(wine_data)
+nrow(wine_data) 
+colnames(wine_data) <- c("Cvs", "Alcohol",
+                         "Malic_Acid", "Ash", "Alkalinity_of_Ash",
+                         "Magnesium", "Total_Phenols", "Flavanoids", "NonFlavanoid_Phenols",
+                         "Proanthocyanins", "Color_Intensity", "Hue", "OD280/OD315_of_Diluted_Wine",
+                         "Proline")
+head(wine_data)
+heatmap(cor(wine_data),Rowv = NA, Colv = NA)
+cultivar_classes <- factor(wine_data$Cvs)
+cultivar_classes
+wine_data_PCA <- prcomp(scale(wine_data[,-1]))
+summary(wine_data_PCA)
