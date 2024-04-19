c2d4a864b566d3fac42d6473fad6393f3289f027
Daniel Chen <85917977+AGreenFruit@users.noreply.github.com>
2024-04-19 18:49:46 -0400

    Added lab10 SVM and Qiskit

---
+++ Labs/Lab10/lab10.py
@@ -0,0 +1,88 @@
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from qiskit_algorithms.utils import algorithm_globals
+from sklearn.svm import SVC
+from qiskit.circuit.library import ZZFeatureMap
+from qiskit.circuit.library import RealAmplitudes
+from qiskit_algorithms.optimizers import COBYLA
+from qiskit.primitives import Sampler
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+import time
+from qiskit_machine_learning.algorithms.classifiers import VQC
+
+
+iris_data = load_iris()
+features = iris_data.data
+labels = iris_data.target
+features = MinMaxScaler().fit_transform(features)
+
+df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+df["class"] = pd.Series(iris_data.target)
+
+sns.pairplot(df, hue="class", palette="tab10")
+
+algorithm_globals.random_seed = 123
+train_features, test_features, train_labels, test_labels = train_test_split(
+    features, labels, train_size=0.8, random_state=algorithm_globals.random_seed
+)
+
+svc = SVC()
+_ = svc.fit(train_features, train_labels)
+
+train_score_c4 = svc.score(train_features, train_labels)
+test_score_c4 = svc.score(test_features, test_labels)
+
+print(f"Classical SVC on the training dataset: {train_score_c4:.2f}")
+print(f"Classical SVC on the test dataset:     {test_score_c4:.2f}")
+
+num_features = features.shape[1]
+
+feature_map = ZZFeatureMap(feature_dimension=num_features, reps=1)
+feature_map.decompose().draw(output="mpl", style="clifford", fold=20)
+
+ansatz = RealAmplitudes(num_qubits=num_features, reps=3)
+ansatz.decompose().draw(output="mpl", style="clifford", fold=20)
+
+optimizer = COBYLA(maxiter=100)
+
+sampler = Sampler()
+
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+
+vqc = VQC(
+    sampler=sampler,
+    feature_map=feature_map,
+    ansatz=ansatz,
+    optimizer=optimizer,
+    callback=callback_graph,
+)
+
+# clear objective value history
+objective_func_vals = []
+
+start = time.time()
+vqc.fit(train_features, train_labels)
+elapsed = time.time() - start
+
+print(f"Training time: {round(elapsed)} seconds")
+
+train_score_q4 = vqc.score(train_features, train_labels)
+test_score_q4 = vqc.score(test_features, test_labels)
+
+print(f"Quantum VQC on the training dataset: {train_score_q4:.2f}")
+print(f"Quantum VQC on the test dataset:     {test_score_q4:.2f}")
